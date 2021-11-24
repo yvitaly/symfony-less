138cd2352191f822b4da8e4656fbd682f7e4dff9
vitalii <yvitalyyy@gmail.com>
2021-11-24 16:49:29 +0200

    change in MainController and add main view

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
-class MainController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
 {
 
+    /**
+     * @Route("/", name="home_page")
+     */
+
+    function indexAction(){
+        return $this->render( view: 'main/index.html.twig');
+    }
 }
